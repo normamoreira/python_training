3a0d821070cd206dc787e3ed09d5adad6254f26f
Norma Elisa Moreira <contavivoitapeva@gmail.com>
2021-05-01 01:10:43 -0400

    Adicionei o ex13

---
+++ exercises/ex13.py
@@ -0,0 +1,7 @@
+h = float(input('Qual a sua altura pessoa :'))
+print('O seu peso ideal é :')
+homen = (72.7*h)-58
+mulher = (62.1*h)-44.7
+print('Para homens :{:.2f} '.format(homen),' quilos')
+
+print('Para mulheres :{:.2f}'.format(mulher),' quilos' )
