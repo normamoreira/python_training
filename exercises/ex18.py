c38686f61a2c788e0f530e4b998ac3e752dcf705
Norma Elisa Moreira <contavivoitapeva@gmail.com>
2021-05-04 15:29:04 -0400

    Estou tendo dificuldade em aplicar laço de repeti

---
+++ exercises/ex18.py
@@ -0,0 +1,29 @@
+numero = [10,20,30,40,50,60,80,90,100]
+for numero in numero :
+    print(numero)
+valor = 0
+for indice in range(5):    
+    valor += 10
+    print(valor)
+for valor in range(5,10):
+    print(valor)    
+for valor in range(0,10,2):
+    print(valor)    
+print('VAMOS PRATICAR')
+
+#Faça um programa que inicialize uma lista de compras com 5 itens diferentes e
+#e exiba todos utilizando um laço de repitição
+print('Digite aqui 5 itens de sua lista de compras')
+primeiro = input('1º :')
+segundo = input('2º :')
+terceiro = input('3º :')
+quarto = input('4º :')
+quinto = input ('5º :')
+
+print('Você deve ir ao mercado e comprar:\
+',primeiro,
+segundo,
+terceiro,
+quarto,
+quinto,)
+
