088e68a8897ee83665c593a4aa1b69e41311d7b1
Norma Elisa Moreira <contavivoitapeva@gmail.com>
2021-05-05 14:27:49 -0400

    Adicionei ex 20

---
+++ exercises/ex20.py
@@ -0,0 +1,4 @@
+#Faça um algoritmo que pergunte ao usuário quantos anos ele tem, depois disso, escreva True no console, caso ele já tenha alcançado a maioridade (18 anos), caso contrário escreva False.
+idade = int(input("Quantos anos você tem ?"))
+maioridade = idade >= 18
+print(maioridade)
