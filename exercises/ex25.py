2a2eb27e7bf09e12bed964603f298dcd3d942dbc
Norma Elisa Moreira <contavivoitapeva@gmail.com>
2021-05-07 11:58:49 -0400

    Adicionei ex 25

---
+++ exercises/ex25.py
@@ -0,0 +1,7 @@
+#Faça um programa que solicite ao usuário 2 valores,
+#utilize uma condição ternária para escrever qual o maior valor:
+#o primeiro ou o segundo (caso os valores sejam iguais,
+#considere o segundo).
+primero_valor = int(input('Digite um valor :'))
+segundo_valor = int(input('Digite um valor :'))
+print('Primeiro'if primero_valor > segundo_valor else 'Segundo')
