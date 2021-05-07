57bcef5801da37dcee38d47523dae27671992af1
Norma Elisa Moreira <contavivoitapeva@gmail.com>
2021-05-07 15:52:49 -0400

    Adicionei ex 27

---
+++ exercises/ex27.py
@@ -0,0 +1,5 @@
+#Faça um programa que solicite uma mensagem qualquer para o usuário e
+#exiba esta mensagem com todas as letras em maiúsculo.
+
+mensagem = input('Digite uma mensagem :')
+print(mensagem.upper())
