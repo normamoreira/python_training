40d70c583a67ed8ea78743ab5feb72b2d7101de9
Norma Elisa Moreira <contavivoitapeva@gmail.com>
2021-05-07 15:48:22 -0400

    Adicionei ex 26

---
+++ exercises/ex26.py
@@ -0,0 +1,5 @@
+#Faça um programa que solicite o nome do usuário e depois disso
+#faça uma saudação no formato: "Olá {nome digitado pelo usuário}".
+
+nome= input('Qual o seu nome ?')
+print('Olá {}'.format(nome))
