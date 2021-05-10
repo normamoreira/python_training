61e823efc88844715810b6da1dc79d72b3118e95
Norma Elisa Moreira <contavivoitapeva@gmail.com>
2021-05-10 13:39:50 -0400

    Adicionei ex 33

---
+++ exercises/ex33.py
@@ -0,0 +1,7 @@
+#Faça um programa que inicialize uma lista de compras com 5 itens 
+#diferentes e exiba todos utilizando um laço de repetição
+
+itens_compra = ['ARROZ','MACARRÃO','AGUA','BOLACHA','OLEO']
+for item in itens_compra:
+    print(item)
+    
